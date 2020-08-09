a918c4ee733c6cc38a22e5efea28676db465245c
hitesh0902 <hiteshrawat.92@gmail.com>
2020-08-09 11:54:18 +0530

    connected to firebase database

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -14,6 +14,5 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-firebase.firestore().settings({ timestampsInSnapshots: true }); // not needed in latest
 
 export default firebase;
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -5,8 +5,24 @@ export const createProject = (project) => {
   //     project: project,
   //   };
 
-  return (dispatch, getState) => {
+  // With thunk for async call to database
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
     // make async call to database
-    dispatch({ type: "CREATE_PROJECT", project });
+    const firestore = getFirestore();
+    firestore
+      .collection("projects")
+      .add({
+        ...project,
+        authorFirstName: "Hitesh",
+        authorLastName: "Rawat",
+        authorId: 12345,
+        createdAt: new Date(),
+      })
+      .then(() => {
+        dispatch({ type: "CREATE_PROJECT", project });
+      })
+      .catch((err) => {
+        dispatch({ type: "CREATE_PROJECT_ERRROR", err });
+      });
   };
 };
